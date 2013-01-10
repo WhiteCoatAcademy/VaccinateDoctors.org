8293fd3d6ed6ef05efdce5e69bb7d7df06e906e7
Nick Semenkovich <semenko@alum.mit.edu>
2013-01-10 16:53:34 -0600

    + Groups / states

--- vyd.py
+++ vyd.py
@@ -49,11 +49,22 @@ def page_index():
 # *** Magic per-state / city / group-targeted URLs
 ######
 
-targets = ['alabama', 'alaska']
+# TODO: Add US territories.
+states = ['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado', 'connecticut', 'delaware', 'dc',
+           'florida', 'georgia', 'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', 'kentucky',
+           'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'mississippi', 'missouri',
+           'montana', 'nebraska', 'nevada', 'new-hampshire', 'new-jersey', 'new-mexico', 'new-york', 'north-carolina',
+           'north-dakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', 'rhode-island',
+           'south-carolina', 'south-dakota', 'tennessee', 'texas', 'utah', 'vermont', 'virginia',
+           'washington', 'west-virginia', 'wisconsin', 'wyoming']
+
+groups = ['moms', 'dads', 'parents', 'patients']
+
+targets = states + groups
 
 @app.route("/<target>/")
 def localized_branding(target):
-    if target in ('moms', 'dads', 'parents', 'patients'):
+    if target in groups:
         return render_template('target_group.html')
     else:
         return render_template('target_state.html')
